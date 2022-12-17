2660cdccba90879fa093eba7a7b5e96d52e49f0c
Abhishek7tech <abhishektab45@gmail.com>
2022-12-17 18:22:01 +0530

    final commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from PIL import Image, ImageFilter
+
+import os, sys
+
+img_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for images in os.listdir(img_folder):
+    img = Image.open(f"./images/{images}")
+    names = os.path.splitext(images)[0]
+    print(names)
+    img.save(f"./new/{names}.png", "png")
